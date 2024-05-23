674b2457127eb4af28d6bb932328f3ff7cabdaa6
haydonge <haydonge@gmail.com>
2024-05-23 09:39:47 +0800

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 import db from "@astrojs/db";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,6 @@
-import { Link, db } from "astro:db";
+import { Link, Stockonhand, Workorder,db } from "astro:db";
 
-// https://astro.build/db/seed
+// // https://astro.build/db/seed
 export default async function seed() {
   await db.insert(Link).values([
     {
@@ -29,4 +29,52 @@ export default async function seed() {
       isRead: false,
     },
   ]);
-}
+  await db.insert(Workorder).values([
+    {
+      title: "My Blog",
+      url: "https://chrispennington.blog",
+      description: "This is my blog about web development and design.",
+      finished: true,
+    },
+    {
+      title: "Google",
+      url: "https://google.com",
+      description: "I found this cool site that can search for anything.",
+      finished: false,
+    },
+    {
+      title: "GitHub",
+      url: "https://github.com",
+      description: "I found this cool site that can hold repositories.",
+      finished: false,
+    },
+    {
+      title: "Twitter",
+      url: "https://twitter.com",
+      description:
+        "I found this cool site that can let you say anything you want without accountability!",
+      finished: false,
+    },
+  ]);
+
+
+await db.insert(Stockonhand).values([
+  {
+    partnumber: "LFNK1000001",
+    description: "This is my blog about web development and design.",  
+    qty:20,
+    url: "https://chrispennington.blog",
+    safeqty:5,
+  },
+  {
+    partnumber: "LFNK1000002",
+    description: "This is my blog about web development and design.",  
+    qty:20,
+    url: "https://sina.com.cn",
+    safeqty:5,
+  },
+  
+]);
+
+};
+
